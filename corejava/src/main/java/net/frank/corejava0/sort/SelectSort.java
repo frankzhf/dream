2c6fac30fe222ebb38a0b37d3760a40595f6bd8b
frank <442526654@qq.com>
2017-04-14 21:20:58 +0800

    Signed-off-by: frank <442526654@qq.com>

--- corejava/src/main/java/net/frank/corejava0/sort/SelectSort.java
+++ corejava/src/main/java/net/frank/corejava0/sort/SelectSort.java
@@ -1,6 +1,6 @@
 package net.frank.corejava0.sort;
 
-public class BubbleSort {
+public class SelectSort {
 	public static void main(String[] args){
 		int [] arr = {6,3,8,2,9,1};
 		System.out.println("排序前数组为：");
@@ -8,16 +8,19 @@ public class BubbleSort {
 			System.out.print(num + "\t");
 		}
 		System.out.println();
-		for(int i=0;i< arr.length -1;i++){
-			for(int j=0;j<arr.length-1-i;j++){
-				if(arr[j] > arr[j+1]){
-					arr[j] = arr[j]+ arr[j+1];
-					arr[j+1] = arr[j] - arr[j+1];
-					arr[j] = arr[j] - arr[j+1];
+		int index;
+		for(int i=1;i<arr.length;i++){
+			index=0;
+			for(int j=1;j<=arr.length -i;j++){
+				if(arr[j] > arr[index]){
+					index = j;
 				}
 			}
+			int temp = arr[arr.length-i];
+			arr[arr.length-i] = arr[index];
+			arr[index] = temp;
 		}
-		System.out.println("排序前数组为：");
+		System.out.println("排序后数组为：");
 		for(int num:arr){
 			System.out.print(num + "\t");
 		}
