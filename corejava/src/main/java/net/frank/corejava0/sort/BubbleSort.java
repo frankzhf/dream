5b2e59961aa8d924807e776351c2bbbd2586f878
zhaofeng <442526654@qq.com>
2017-04-14 17:38:06 +0800

    笔记

---
+++ corejava/src/main/java/net/frank/corejava0/sort/BubbleSort.java
@@ -0,0 +1,26 @@
+package net.frank.corejava0.sort;
+
+public class BubbleSort {
+	public static void main(String[] args){
+		int [] arr = {6,3,8,2,9,1};
+		System.out.println("排序前数组为：");
+		for(int num:arr){
+			System.out.println(num);
+		}
+		for(int i=0;i< arr.length -1;i++){
+			for(int j=0;j<arr.length-1-i;j++){
+				if(arr[j] > arr[j+1]){
+					arr[j] = arr[j]+ arr[j+1];
+					arr[j+1] = arr[j] - arr[j+1];
+					arr[j] = arr[j] - arr[j+1];
+				}
+			}
+		}
+		System.out.println("排序前数组为：");
+		for(int num:arr){
+			System.out.println(num);
+		}
+		
+		
+	}
+}
