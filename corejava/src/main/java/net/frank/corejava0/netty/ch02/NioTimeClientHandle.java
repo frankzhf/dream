12bc1d70353633b0599ddb5d472c94c6e15ecd8f
frank <442526654@qq.com>
2017-09-12 08:40:35 +0800

    nio基础客户端

--- corejava/src/main/java/net/frank/corejava0/netty/ch02/NioTimeClientHandle.java
+++ corejava/src/main/java/net/frank/corejava0/netty/ch02/NioTimeClientHandle.java
@@ -1,8 +1,13 @@
 package net.frank.corejava0.netty.ch02;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 
 public class NioTimeClientHandle implements Runnable {
 	
@@ -31,8 +36,97 @@ public class NioTimeClientHandle implements Runnable {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		try{
+			doConnect();
+		}catch(IOException e){
+			e.printStackTrace();
+			System.exit(1);
+		}
+		while(!stop){
+			try{
+				selector.select(1000);
+				Set<SelectionKey> selectedKeys = selector.selectedKeys();
+				Iterator<SelectionKey> it = selectedKeys.iterator();
+				SelectionKey key = null;
+				while(it.hasNext()){
+					key = it.next();
+					it.remove();
+					try{
+						handleInput(key);
+					}catch(Exception e){
+						e.printStackTrace();
+					}finally{
+						if(key!=null){
+							key.cancel();
+							if(key.channel()!=null){
+								key.channel().close();
+							}
+						}
+					}
+				}
+			
+			}catch(Exception e){
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
+		if(selector!=null){
+			try{
+				selector.close();
+			}catch(IOException e){
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void handleInput(SelectionKey key) throws IOException {
+		if(key.isValid()){
+			SocketChannel sc = (SocketChannel) key.channel();
+			if(key.isConnectable()){
+				if(sc.finishConnect()){
+					sc.register(selector, SelectionKey.OP_READ);
+					doWrite(sc);
+				}else{
+					System.exit(1);
+				}
+			}
+			if(key.isReadable()){
+				ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+				int readBytes = sc.read(readBuffer);
+				if(readBytes>0){
+					readBuffer.flip();
+					byte[] bytes = new byte[readBuffer.remaining()];
+					readBuffer.get(bytes);
+					String body = new String(bytes,"UTF-8");
+					System.out.println("Now is : " + body);
+					this.stop = true;
+				}else if(readBytes<0){
+					key.cancel();
+					sc.close();
+				}else
+					;
+			}
+		}
+	}
+
+	private void doConnect()throws IOException{
+		if(socketChannel.connect(new InetSocketAddress(host,port))){
+			socketChannel.register(selector, SelectionKey.OP_READ);
+			doWrite(socketChannel);
+		}else{
+			socketChannel.register(selector, SelectionKey.OP_CONNECT);
+		}
+	}
+	
+	private void doWrite(SocketChannel sc)throws IOException{
+		byte[] req = "QUERY TIME ORDER".getBytes();
+		ByteBuffer writeBuffer = ByteBuffer.allocate(req.length);
+		writeBuffer.put(req);
+		writeBuffer.flip();
+		sc.write(writeBuffer);
+		if(!writeBuffer.hasRemaining()){
+			System.out.println("Send order 2 server succeed.");
+		}
 	}
 
 }
