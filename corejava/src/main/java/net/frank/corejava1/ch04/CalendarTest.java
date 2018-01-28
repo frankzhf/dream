728939f6244de90f61737f921ea2bbca7e73bd6e
zhaofeng <zhaofeng@192.168.1.7>
2018-01-28 21:13:22 +0800

    Calendar类

--- corejava/src/main/java/net/frank/corejava1/ch04/CalendarTest.java
+++ corejava/src/main/java/net/frank/corejava1/ch04/CalendarTest.java
@@ -70,5 +70,55 @@ public class CalendarTest {
 			System.out.println();
 		}
 	}
-	
+	/***
+	 * java.util.GregorianCalendar 格里高利历
+	 * GregorianCalendar()
+	 * 构造一个日历对象，用来表示默认地区，默认时区的当前时间
+	 * 
+	 * GregorianCalendar(int year, int month, int day)
+	 * GregorianCalendar(int year, int month, int day, int hour, int minute, int secord)
+	 * 用给定日期和时间构造一个日历对象
+	 * 参数	year				该日期所在年份
+	 * 		month			该日期所在月份		以0为基准, 0表示1月
+	 * 		day				该月份中的日期
+	 * 		hour				小时（0-23）
+	 * 		minute			分钟（0-59）
+	 * 		secord			秒（0-59）
+	 * 
+	 * int get(int field)
+	 * 返回给定域的值			可以是下述选项之一：	Calendar.ERA、Calendar.YEAR、Calendar.MONTH、
+	 * 											Calendar.WEEK_OF_YEAR、Calendar_WEEK_OF_MONTH、
+	 * 											Calendar.DAY_OF_MONTH、Calendar.DAY_OF_YEAR、
+	 * 											Calendar.DAY_OF_WEEK、Calendar.DAY_OF_WEEK_IN_MONTH、
+	 * 											Calendar.AM_PM、Calendar.HOUR、Calendar.HOUR_OF_DAY、
+	 * 											Calendar.MINUTE、Calendar.SECOND、
+	 * 											Calendar.MILLISECOND、Calendar.ZONE_OFFSET、Calendar.DST_OFFSET
+	 *
+	 * void set(int field, int value)
+	 * 设置给定域的值
+	 * 
+	 * void set(int year, int month, int day)
+	 * void set(int year, int month, int day, int hour, int minute, int secord)
+	 * 将日期域和时间域设置为新值
+	 * 
+	 * void add(int field, int amount)
+	 * 可以对日期信息进行算术运算，amount可以是负值
+	 * 
+	 * int getFirstDayOfWeek()
+	 * 获取当前用户所在地区，一个星期中的第一天。
+	 * 
+	 * void setTime(Date time)
+	 * 将日历设置成指定时间点
+	 * 
+	 * Date getTime()
+	 * 获取这个日历当前值所表达的时间点
+	 * 
+	 * java.text.DateFormatSymbols
+	 * String[] getShortWeekdays()
+	 * String[] getShortMontsh()
+	 * String[] getWeekdays()
+	 * String[] getMonths()
+	 * 获取当前地区的星期几或月份的名称
+	 * 
+	 */
 }
