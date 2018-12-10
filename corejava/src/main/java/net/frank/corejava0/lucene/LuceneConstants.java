955b16624e52cb0866e7f65176bd327bb9a3905b
zhaofeng <zhaofeng@192.168.31.55>
2018-12-10 13:52:12 +0800

    移动资源

--- corejava/src/main/java/net/frank/corejava0/lucene/LuceneConstants.java
+++
@@ -1,6 +0,0 @@
-package net.frank.corejava0.lucene;
-
-public interface LuceneConstants {
-	String DEFAULT_DATA_DIR = "/Users/zhaofeng/projects/frank/source/dream/corejava/src/lucene/data";
-	String DEFAULT_INDEX_DIR = "/Users/zhaofeng/projects/frank/source/dream/corejava/src/lucene/index";
-}
